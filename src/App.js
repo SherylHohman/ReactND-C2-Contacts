d05e86321cc2369a2fa8ef20a502bbdbd3ddba58
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-21 10:41:03 -0800

    replace BoilerPlate UI with ContactList - my first React Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends Component {
+  render(){
+    const people = [
+      {name: "Sheryl"},
+      {name: "Michael"},
+      {name: "Jess"},
+      {name: "Clint Kerr"}
+    ];
+
+    return (
+      <ol>
+        {people.map( person => <li key={person.name}>{person.name}</li> )}
+      </ol>
+      );
+  };
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Contacts App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         <i>This is 'ContactList' - My First React Component:</i>
         </p>
+        <ContactList />
       </div>
     );
   }
