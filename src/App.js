2d994db8f6e14161f60f21497ddd1614b3269486
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-22 15:22:52 -0800

    add temp contacts constant, until we learn to use the provided ContactsAPI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 
+// Temp contacts list, until we learn how to make network requests, using:
+// - src/utils/ContactsAPI.js, our preconfigured contacts API
+// - with our preconfigured Backend Server
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
+
 class App extends Component {
   render() {
     return (
