5d22989a78a82ba93d778242f9e0cabb980fa742
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-23 15:24:55 -0800

    change ListContacts to Functional Stateless Component
    was (regular class-based) Component
    
    Lesson 3, Module 3: Functional Components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,26 +1,57 @@
 import React, {Component} from 'react';
 
-class ListContacts extends Component {
-  render(){
+function ListContacts(props){
     return (
-        <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
-            <li key={contact.id} className='contact-list-item'>
-              <div className='contact-avatar' style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}/>
-              <div className='contact-details'>
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button className='contact-remove'>
-                Remove
-              </button>
-            </li>
-          ))}
-        </ol>
-      );
+      <ol className='contact-list'>
+        {props.contacts.map((contact) => (
+          <li key={contact.id} className='contact-list-item'>
+            <div className='contact-avatar' style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className='contact-details'>
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className='contact-remove'>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    );
   };
-}
+
+  // Since ListContacts ONLY has a RenderMethod, and No State,
+  //   we can use a Functional Stateless Component, rather than a React Component.
+  //   - props is passed in as the first argument to the function, and
+  //   - we no longer access it (props) using the "this" keyword.
+  //     (since this is not a class)
+  //   - We use "function" instead of "class..extends Component" syntax.
+  
+
+// For comparative purposes, Below is the Original Component class version:
+
+// class ListContacts extends Component {
+//   render(){
+//     return (
+//         <ol className='contact-list'>
+//           {this.props.contacts.map((contact) => (
+//             <li key={contact.id} className='contact-list-item'>
+//               <div className='contact-avatar' style={{
+//                 backgroundImage: `url(${contact.avatarURL})`
+//               }}/>
+//               <div className='contact-details'>
+//                 <p>{contact.name}</p>
+//                 <p>{contact.email}</p>
+//               </div>
+//               <button className='contact-remove'>
+//                 Remove
+//               </button>
+//             </li>
+//           ))}
+//         </ol>
+//       );
+//   };
+// }
 
 export default ListContacts
