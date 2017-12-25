91dcfd26ff1a938f894561e1b3200fad4d5d8b30
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-24 19:48:03 -0800

    syn: remove comments that document the learning process
    Lesson3, Modules 3, 4, 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
 import ListContacts from "./ListContacts.js";
 
-// Temp contacts list, until we learn how to make network requests, using:
-// - src/utils/ContactsAPI.js, our preconfigured contacts API
-// - with our preconfigured Backend Server
-
-// L3, Module 4: Managing State
-// Let App manage state of contacts list (so can delete and add new)
-//   by moving it inside App Component, as a field in its 'state' "field variable"
-
-
 class App extends Component {
 
   state = {
-    // const contacts = [
     contacts : [
       {
         "id": "ryan",
@@ -34,28 +24,13 @@ class App extends Component {
         "avatarURL": "http://localhost:5001/tyler.jpg"
       }
     ]
-
   }
 
-  // L3, Module 5: "Update State with SetState"
   removeContact = (contact) => {
-    // takes in (the) contact that was clicked on (we'll access contact.id)
-    // pass it a function.. that returns an object. See below for how syntax for this evolves
-    // this.setState();             the bar function call
-    // this.setState( () => () );   .. that's passed in a function
-    // this.setState( () => ({}) ); .. that returns an object
     this.setState( (prevState) => ({
-      // contacts is now a OBJECT on state.
-      // We set "contacts: ..." to a new (array) as defined below (ie minus current contact)
-      // state is the variable passed in.  Don't use this.state.contacts
-      // updated the var passed in to "prevState", to make this point clearer.
       contacts: prevState.contacts.filter((contacts_old) => (
-        // filter function is also being passed in a function hence the (()=>()) syntax
-        // remove the contact that was clicked on
-        contacts_old.id != contact.id
-        // NO ; at the end of above line, its an Object (a , would be used if there was another key/value pair)
-        // filter out the former contact that has an id of the contact which was clicked on (this contact is passed back to us from ListContacts OnClick function via "contact" that's passed into our removeContact function)
-      ))
+         contacts_old.id != contact.id
+       ))
     }) );
   }
 
@@ -69,38 +44,4 @@ class App extends Component {
 }
 
 export default App;
-
-
-// Notes for L3, Module 4: Add State to a Component
-  // contacts array is now a "state" variable of the App Component
-  // App is now a Component with state instead of a Stateless Component.
-  // data that is passed to child components should be encapsulated by that component.  Previously, it was just a random variable inside App.js, but not owned by anyone.
-  // Notice that:
-    //          <ListContacts contacts={contacts}/>
-    // becomes :
-    //\        <ListContacts contacts={this.state.contacts}/>
-    // and that contacts is now a PROPERTY of the state OBJECT, not a plain variable (constant):
-    //          const contacts = [
-    // becomes
-    //          contacts : [
-    // within the state = {} object
-
-// Notes for L3, Module 5: Update state with setState
-  //  App component owns the contacts array
-  //  create a method. removeContact, on App that modifies the contact array
-  // it does this with the pattern of passing in a function which takes in state, and MODIFIES state.
-  // it returns a new state variable with a new value for contacts
-  // its new value is the original array of contacts, MINUS the contact that had the contact.id of the clicked on contact.
-  // this new state variable is MERGED with the Previous value of the state variable (automatically by react).  This distinction of MERGING old state{} object with new state{} object is probably so we only need to include code for the parts were CHANGING, not list the current value of ALL (key/value pairs of) variables that are part of the defined state Object.
-
-  // this removeContact method must be passed to ListContacts via props
-
-  // How does it know which contact that is?
-    //  we pass removeContact() method as a prop to ListContacts Component
-    // we reference this method within ListContacts as onDeleteContact() (chosen since it's triggered by an onclick handler)
-    // we add an onClick handler to our (delete) button within that li
-    // that li references the contact that's been clicked on for deletion
-    // this contact is passed into App.removeContact(contact)
-    // which then calls setstate...
-
 
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,18 @@
 import React from 'react';
 
-// import React, { Component } from 'react';
-// we don't need to import {Component},
-// now that we've refatored to use a Functional Stateless Component
-
-// COMPARE traditional ES5 function syntax... to ES6 Arrow Function Syntax..
-
-// function ListContacts(props){
-    // return (
-
-// ES6 arrow function syntax:
 const ListContacts = (props) => (
       <ol className='contact-list'>
         {props.contacts.map((contact) => (
           <li key={contact.id} className='contact-list-item'>
             <div className='contact-avatar' style={{
+              /* uses Template Literal backticks notation*/
               backgroundImage: `url(${contact.avatarURL})`
             }}/>
             <div className='contact-details'>
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
-            {/* invoke onDeleteContact (App.removeContact) whenever this button is clicked on*/}
-            {/* odd syntax for defining the arrow function that onClick invokes. ()=>fn_name()  */}
-            {/* Lesson3 Module 5: Updating State with setState */}
+            {/* X button to delete a contact */}
             <button onClick={()=>props.onDeleteContact(contact)} className='contact-remove'>
               Remove
             </button>
@@ -31,62 +20,5 @@ const ListContacts = (props) => (
         ))}
       </ol>
     );
-  // };
-
-// traditional ES5 function syntax...
-
-// function ListContacts(props){
-//     return (
-//       <ol className='contact-list'>
-//         {props.contacts.map((contact) => (
-//           <li key={contact.id} className='contact-list-item'>
-//             <div className='contact-avatar' style={{
-//               backgroundImage: `url(${contact.avatarURL})`
-//             }}/>
-//             <div className='contact-details'>
-//               <p>{contact.name}</p>
-//               <p>{contact.email}</p>
-//             </div>
-//             <button className='contact-remove'>
-//               Remove
-//             </button>
-//           </li>
-//         ))}
-//       </ol>
-//     );
-//   };
-
-  // Since ListContacts ONLY has a RenderMethod, and No State,
-  //   we can use a Functional Stateless Component, rather than a React Component.
-  //   - props is passed in as the first argument to the function, and
-  //   - we no longer access it (props) using the "this" keyword.
-  //     (since this is not a class)
-  //   - We use "function" instead of "class..extends Component" syntax.
-
-
-// For comparative purposes, Below is the Original Component class version:
-
-// class ListContacts extends Component {
-//   render(){
-//     return (
-//         <ol className='contact-list'>
-//           {this.props.contacts.map((contact) => (
-//             <li key={contact.id} className='contact-list-item'>
-//               <div className='contact-avatar' style={{
-//                 backgroundImage: `url(${contact.avatarURL})`
-//               }}/>
-//               <div className='contact-details'>
-//                 <p>{contact.name}</p>
-//                 <p>{contact.email}</p>
-//               </div>
-//               <button className='contact-remove'>
-//                 Remove
-//               </button>
-//             </li>
-//           ))}
-//         </ol>
-//       );
-//   };
-// }
 
 export default ListContacts
