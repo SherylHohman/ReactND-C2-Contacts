3aa54a0cfaf94832dfbe721e7a18a0c3f006f242
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-24 18:00:52 -0800

    add removeContact method to App Component to change its state
    state on App.js consists soley of the contacts array.
    removeContact will "delete" a contact from the array
      by returning a new state variable with its contacts "key" to be
      defined as the previous contacts array, devoid of the selected contact
    
    Lesson 3, Module 5: Updating State with setState

--- src/App.js
+++ src/App.js
@@ -57,6 +57,27 @@ class App extends Component {
 
   }
 
+  // Lesson 3, Module 5: "Update State with SetState"
+  removeContact = (contact) => {
+    // takes in (the) contact that was clicked on (we'll access contact.id)
+    // pass it a function.. that returns an object. See below for how syntax for this evolves
+    // this.setState();             the bar function call
+    // this.setState( () => () );   .. that's passed in a function
+    // this.setState( () => ({}) ); .. that returns an object
+    this.setState( (state) => ({
+      // contacts is now a OBJECT on state.
+      // We set "contacts: ..." to a new (array) as defined below (ie minus current contact)
+      // state is the variable passed in.  Don't use this.state.contacts
+      contacts: state.contacts.filter((contacts_old) => (
+        // filter function is also being passed in a function hence the (()=>()) syntax
+        // remove the contact that was clicked on
+        contacts_old.id != contact.id
+        // NO ; at the end of above line, its an Object (a , would be used if there was another key/value pair)
+        // filter out the former contact that has an id of the contact which was clicked on (this contact is passed back to us from ListContacts OnClick function via "contact" that's passed into our removeContact function)
+      ))
+    }) );
+  }
+
   render() {
     return (
       <div>
@@ -70,18 +91,26 @@ export default App;
 
 
 // Notes for Lesson 3, Module 4: Add State to a Component
+  // contacts array is now a "state" variable of the App Component
+  // App is now a Component with state instead of a Stateless Component.
+  // data that is passed to child components should be encapsulated by that component.  Previously, it was just a random variable inside App.js, but not owned by anyone.
+  // Notice that:
+    //          <ListContacts contacts={contacts}/>
+    // becomes :
+    //\        <ListContacts contacts={this.state.contacts}/>
+    // and that contacts is now a PROPERTY of the state OBJECT, not a plain variable (constant):
+    //          const contacts = [
+    // becomes
+    //          contacts : [
+    // within the state = {} object
 
-// contacts array is now a "state" variable of the App Component
-// App is now a Component with state instead of a Stateless Component.
-// data that is passed to child components should be encapsulated by that component.  Previously, it was just a random variable inside App.js, but not owned by anyone.
-// Notice that:
-  //          <ListContacts contacts={contacts}/>
-  // becomes :
-  //\        <ListContacts contacts={this.state.contacts}/>
-  // and that contacts is now a PROPERTY of the state OBJECT, not a plain variable (constant):
-  //          const contacts = [
-  // becomes
-  //          contacts : [
-  // within the state = {} object
+// Notes for Lesson 3, Module 5: Update state with setState
+  //  App component owns the contacts array
+  //  create a method. removeContact, on App that modifies the contact array
+  // it does this with the pattern of passing in a function which takes in state, and MODIFIES state.
+  // it returns a new state variable with a new value for contacts
+  // its new value is the original array of contacts, MINUS the contact that had the contact.id of the clicked on contact.
 
+  // How does it know what contact that is?
+    //  
 
