2ae91b637a3d4f1390d88326183265c0c3284ba6
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-24 22:10:01 -0800

    refactor: ListContacts to be a Component with State
    so we can add a query string to UI's state to filter which contacts are visible
    
    the refactor also requires we move propTypes inside the class as a static property
    
    L3, Module 7: Controlled Components
    (will add a Search Bar used to filter ListContacts)

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,102 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-// import React, { Component } from 'react';
-// we don't need to import {Component},
-// now that we've refatored to use a Functional Stateless Component
+class ListContacts extends Component {
 
-// COMPARE traditional ES5 function syntax... to ES6 Arrow Function Syntax..
+  // propTypes is now a static property on our class,
+  // after refactor from Functional Stateless Component
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  };
 
-// function ListContacts(props){
-    // return (
 
-// ES6 arrow function syntax:
-const ListContacts = (props) => (
+  render() {
+    return (
       <ol className='contact-list'>
-        {props.contacts.map((contact) => (
+
+        {this.props.contacts.map((contact) => (
           <li key={contact.id} className='contact-list-item'>
+
             <div className='contact-avatar' style={{
               backgroundImage: `url(${contact.avatarURL})`
             }}/>
+
             <div className='contact-details'>
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
+
             {/* invoke onDeleteContact (App.removeContact) whenever this button is clicked on*/}
             {/* odd syntax for defining the arrow function that onClick invokes. ()=>fn_name()  */}
             {/* Lesson3 Module 5: Updating State with setState */}
-            <button onClick={()=>props.onDeleteContact(contact)} className='contact-remove'>
+            <button onClick={()=>this.props.onDeleteContact(contact)} className='contact-remove'>
               Remove
             </button>
+
           </li>
         ))}
+
       </ol>
     );
-  // };
+  }}
 
-// add a PROPERTY, propTypes, to our stateless Functional Component
+// refactor from Functional Stateless Component to Component with state
+//   requires we move propTypes inside the class, as a static property
+//   see above; remove below.
+
+// L3, Module 6: PropTypes
+// add a PROPERTY, propTypes ot our ListContacts Component
 // note that:
 // - the component's Property has a lower-case p
 // - the class PropTypes (we imported) has an Upper-case P
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-};
 
-// traditional ES5 function syntax...
+// ListContacts.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onDeleteContact: PropTypes.func.isRequired
+// };
 
-// function ListContacts(props){
-//     return (
+
+
+// // COMPARE with BELOW: as a Stateless FUNCTIONAL Component
+
+// // import React from 'react';
+// // we don't need to import {Component},
+// // when using a Functional Stateless Component
+
+// // Compare next two lines
+// //   traditional ES5 function syntax... to ES6 Arrow Function Syntax..
+// // function ListContacts(props){
+//     // return ( ...
+
+// // to ES6 arrow function syntax:
+// const ListContacts = (props) => (
 //       <ol className='contact-list'>
 //         {props.contacts.map((contact) => (
 //           <li key={contact.id} className='contact-list-item'>
+
 //             <div className='contact-avatar' style={{
 //               backgroundImage: `url(${contact.avatarURL})`
 //             }}/>
+
 //             <div className='contact-details'>
 //               <p>{contact.name}</p>
 //               <p>{contact.email}</p>
 //             </div>
-//             <button className='contact-remove'>
+
+//             {/* invoke onDeleteContact (App.removeContact) whenever this button is clicked on*/}
+//             {/* odd syntax for defining the arrow function that onClick invokes. ()=>fn_name()  */}
+//             {/* Lesson3 Module 5: Updating State with setState */}
+//             <button onClick={()=>props.onDeleteContact(contact)} className='contact-remove'>
 //               Remove
 //             </button>
+
 //           </li>
 //         ))}
 //       </ol>
 //     );
-//   };
+//   // };
 
-  // Since ListContacts ONLY has a RenderMethod, and No State,
-  //   we can use a Functional Stateless Component, rather than a React Component.
-  //   - props is passed in as the first argument to the function, and
-  //   - we no longer access it (props) using the "this" keyword.
-  //     (since this is not a class)
-  //   - We use "function" instead of "class..extends Component" syntax.
+// // L3, Module 6: PropTypes
+// // add a PROPERTY, propTypes ot our ListContacts Component
+// // note that:
+// // - the component's Property has a lower-case p
+// // - the class PropTypes (we imported) has an Upper-case P
+// ListContacts.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onDeleteContact: PropTypes.func.isRequired
+// };
 
-
-// For comparative purposes, Below is the Original Component class version:
-
-// class ListContacts extends Component {
-//   render(){
-//     return (
-//         <ol className='contact-list'>
-//           {this.props.contacts.map((contact) => (
-//             <li key={contact.id} className='contact-list-item'>
-//               <div className='contact-avatar' style={{
-//                 backgroundImage: `url(${contact.avatarURL})`
-//               }}/>
-//               <div className='contact-details'>
-//                 <p>{contact.name}</p>
-//                 <p>{contact.email}</p>
-//               </div>
-//               <button className='contact-remove'>
-//                 Remove
-//               </button>
-//             </li>
-//           ))}
-//         </ol>
-//       );
-//   };
-// }
 
 export default ListContacts
+
+
+// L3, Module 7: Controlled Components
+// This Module adds a Search Form to ListContacts UI
+// The Search Form will
+//  - filter ListContacts UI based on query typed in by user to
+//    display only contacts matching the query string
+//  - adds a section indicating how many contacts have been filtered out by the query string,
+//    and a "show All" button that removes the query filter
+
+// 1. Search query string adds STATE to our component,
+//    so we need to refactor our Stateless Functional Component (back)
+//    into a Component (with state)
