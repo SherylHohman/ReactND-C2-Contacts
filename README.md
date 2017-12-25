1295aff21f7da173ccbf52ea791119181af91568
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-24 16:48:40 -0800

    Refactor contacts array to App Component's State variable
    Lesson 3, Module 4: Add State to a Component

--- README.md
+++ README.md
@@ -17,4 +17,32 @@ Repo for the Backend Server is https://github.com/SherylHohman/ReactND-C2L2-Cont
 
 ##### Front-end Development server: Accessible on ****port 3000 (with  `yarn start` or `npm start`)  
 
+---
+
+#### Notes
+##### Lesson 3, Module 4: Add State To A Component (notes from..)
+State
+`props` refer to `attributes` from parent components. In the end, props represent "read-only" data that are immutable.
+
+A component's **state**, on the other hand, represents **mutable data** that ultimately affects what is **rendered** on the page. 
+**State is managed internally by the component itself** and is meant to change over time, commonly due to user input (e.g., clicking on a button on the page).
+
+How we  encapsulate the complexity of state management to individual components, is by adding an **object** called **`state`** to a `Component` 
+
+        class ComponentName() extends React.Component) {
+            state: {
+                'name': 'Sheryl'
+            }
+
+            render() {
+                return (
+                    <p> Name: {this.state.name} </p>
+                )
+            }
+        }
+
+Note: the `state` object goes directly inside the *class*, NOT in the *constructor* method.
+This is called a `class field`.
+
+Basically, `state` are variables that we want a specific React Component to manage, as these variables represent (changing) data that the Component will be rendering to the UI/screen.
 
--- src/App.js
+++ src/App.js
@@ -4,35 +4,84 @@ import ListContacts from "./ListContacts.js";
 // Temp contacts list, until we learn how to make network requests, using:
 // - src/utils/ContactsAPI.js, our preconfigured contacts API
 // - with our preconfigured Backend Server
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
+
+// Lesson 3, Module 4: Managing State
+// Let App manage state of contacts list (so can delete and add new)
+//   by moving it inside App Component, as a field in its 'state' "field variable"
+
+// const contacts = [
+//   {
+//     "id": "ryan",
+//     "name": "Ryan Florence",
+//     "email": "ryan@reacttraining.com",
+//     "avatarURL": "http://localhost:5001/ryan.jpg"
+//   },
+//   {
+//     "id": "michael",
+//     "name": "Michael Jackson",
+//     "email": "michael@reacttraining.com",
+//     "avatarURL": "http://localhost:5001/michael.jpg"
+//   },
+//   {
+//     "id": "tyler",
+//     "name": "Tyler McGinnis",
+//     "email": "tyler@reacttraining.com",
+//     "avatarURL": "http://localhost:5001/tyler.jpg"
+//   }
+// ]
 
 class App extends Component {
+
+  state = {
+    // const contacts = [
+    contacts : [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
      </div>
     );
   }
 }
 
 export default App;
+
+
+// Notes for Lesson 3, Module 4: Add State to a Component
+
+// contacts array is now a "state" variable of the App Component
+// App is now a Component with state instead of a Stateless Component.
+// data that is passed to child components should be encapsulated by that component.  Previously, it was just a random variable inside App.js, but not owned by anyone.
+// Notice that:
+  //          <ListContacts contacts={contacts}/>
+  // becomes :
+  //\        <ListContacts contacts={this.state.contacts}/>
+  // and that contacts is now a PROPERTY of the state OBJECT, not a plain variable (constant):
+  //          const contacts = [
+  // becomes
+  //          contacts : [
+  // within the state = {} object
+
+
