e16f0a9cfa6494b63ac2cc4c05ac4fa72efd6aee
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-25 22:00:46 -0800

    add componentDidMount to fetch contacts with AJAX
    uses ContactsAPI to make AJAX request from backend server at port 5001
    componentDidMount calls method from ContactsAPI to make the request
      .then calls this.setState to populate state with contacts
    
    hardcoded contacts have been deleted from App.js {contacts : []}
    
    L4, Module 2: componentDidMount Lifecycle Event

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
 import ListContacts from "./ListContacts.js";
+import * as ContactsAPI from "./utils/ContactsAPI.js";
 
 class App extends Component {
 
   state = {
-    contacts : [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    // contacts will now be fetched from DB via ContactsAPI
+    // L4 Module2: ComponentDidMountLifecycleEvent
+    contacts : []
+  }
 
+  componentDidMount() {
+    // getAll returns a Promise, so call .then on it.
+    // the .then function is going to be invoked with our contacts
+    //  (see API - it gives us data.contacts from the AJAX call)
+    // now out .then function will call setState, giving it our contacts property
+    // NOTE ES6 syntax: { contaacts }
+    // is equivalent to { contacts: contacts}
+
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts }) ;         // ES6 syntax
+  // this.setState({ contacts: contacts});  // ES5 syntax
+    })
   }
 
   removeContact = (contact) => {
