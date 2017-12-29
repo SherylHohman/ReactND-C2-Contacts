98076c364bd84045e09043f59083608d7b70b475
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-28 19:44:13 -0800

    feat: make the contact form, inc avatar image upload
    InputImage component supplied by instructor.
    It uploads an avatar from user's computer and resizes the image prperly
    
    - L5_ReactRouter, 6.Finishing the Contact Form, Vid-1: The Contact Form
    -

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 // create dummy "create contact" "screen" ie component.
 // at this time it does nothing but renter, so.. start with a
@@ -6,7 +8,21 @@ import React from 'react';
 
 const CreateContact = function(props) {
   return (
-    <div className="create-contact-form">-- Add a New Contact --</div>
+    <div>
+      <Link to="/" className="close-create-contact">Back/CancelClose</Link>
+      <form className="create-contact-form">
+        <ImageInput
+          className="create-contact-avatar-input"
+          name="avatarURL"
+          maxHeight={64}
+        />
+        <div className="create-contact-details">
+          <input type="text" name="name" placeholder="Name"/>
+          <input type="text" name="email" placeholder="Email"/>
+          <button className="create-contact-details">Add Contact</button>
+        </div>
+      </form>
+    </div>
   )
 };
 
